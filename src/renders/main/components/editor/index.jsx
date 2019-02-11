56bfbb4aea97917ab4640dfffb6f9bea5b3bc2bf
huayun.mc <huayun.mc@alibaba-inc.com>
2019-02-11 18:22:43 +0800

    update

--- src/renders/main/components/editor/index.jsx
+++ src/renders/main/components/editor/index.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import ReactMarkdown from 'react-markdown';
+import AceEditor from 'react-ace';
 import './index.scss';
 
+import 'brace/mode/markdown';
+import 'brace/theme/github';
 export default class Editor extends React.Component {
   render() {
     return (
-      <div className="editor-panel">
-        <ReactMarkdown />
+      <div className="editor-component-container">
+        <AceEditor
+          mode="markdown"
+          width="100%"
+          height="100%"
+          theme="github"
+          fontSize={14}
+          wrapEnabled={true}
+          highlightActiveLine={false}
+          showPrintMargin={false}
+          tabSize={2}
+          showGutter={false}
+          maxLines={Infinity}
+          style={{
+            minHeight: 100,
+            backgroundColor: 'transparent'
+          }} />
       </div>
     )
   }
